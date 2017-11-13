a9fa74d191cb42aa7e992f0c86f62748a0e046e7
Rachel Pearse <rpsongs33@gmail.com>
2017-11-13 07:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//const currentUser = 'Grace Hopper';
-//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-`excitedWelcomeMessage`
+
